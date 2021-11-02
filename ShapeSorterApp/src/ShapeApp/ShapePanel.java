e65cfe083a9e6176462644de744c7014e4c43cc6
isaiahL88 <isaiah.ib@outlook.com>
2021-11-02 00:07:19 -0400

    Finsing Correct implimentation of Shape Panel and Sorting Technique

--- ShapeSorterApp/src/ShapeApp/ShapePanel.java
+++ ShapeSorterApp/src/ShapeApp/ShapePanel.java
@@ -14,12 +14,26 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class ShapePanel extends JPanel implements ActionListener{
+	private List<Shape> shapes;
+	private JButton loadShapes;
+	private JButton sortShapes;
 	
 	public ShapePanel() {
+		shapes = new ArrayList<Shape>();
+		this.loadShapes = new JButton("Load Shapes");
+		this.sortShapes = new JButton("Sort Shapes");
+		this.loadShapes.addActionListener(this);
+		this.sortShapes.addActionListener(this);
+		this.add(this.loadShapes);
+		this.add(this.sortShapes);
 	}
 	
 	private List<Shape> createShapes(){
 		List<Shape> shapeList = new ArrayList<Shape>();
+		
+		Color myColor3 = new Color(131, 121, 44);
+		Shape s3= ShapeFactory.getShapeInstance("Rectangle", 150, 150, 80, 80, myColor3);
+		shapeList.add(s3);
 		
 		Color myColor1 = new Color(231, 151, 44);
 		Shape s1 = ShapeFactory.getShapeInstance("Rectangle", 50, 150, 44, 48, myColor1);
@@ -29,22 +43,21 @@ public class ShapePanel extends JPanel implements ActionListener{
 		Shape s2= ShapeFactory.getShapeInstance("Rectangle", 150, 150, 72, 45, myColor2);
 		shapeList.add(s2);
 		
+		
 		return shapeList;
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
-		
-		List<Shape> shapes = createShapes();
-		
+				
 		int x = 0;
-		int y = 0;
-		for (Shape s: shapes) {
+		int y = 50;
+		for (Shape s: this.shapes) {
 			g2d.setColor(s.getShapeColor());
 			
 	    	s.drawShape(g2d, x, y);
-	    	x += 50;
+	    	x += 20 + s.getWidth();
 		}
 	} 
 	
@@ -52,11 +65,8 @@ public class ShapePanel extends JPanel implements ActionListener{
 		ShapePanel panel = new ShapePanel();
 		JFrame frame = new JFrame("Display a rectangle");
 		JLabel label = new JLabel("number of clicks: 0");
-		JButton button = new JButton("");
-		button.addActionListener(panel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
-		panel.add(button);
 		panel.add(label);
 		frame.setSize(360, 300);
 		frame.setLocationRelativeTo(null);
@@ -65,7 +75,13 @@ public class ShapePanel extends JPanel implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
+		if(e.getSource() == this.loadShapes) {
+			this.shapes = createShapes();
+			repaint();
+		}else if(e.getSource() == this.sortShapes) {
+			this.shapes = SortingTechnique.sortShapes(this.shapes);
+			repaint();
+		}
 	}
 	
 	
--- ShapeSorterApp/src/ShapeApp/SortingTechnique.java
+++ ShapeSorterApp/src/ShapeApp/SortingTechnique.java
@@ -4,7 +4,38 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class SortingTechnique {
-	public List<Shape> sortShapes(ArrayList<Shape> shapes){
+	public static List<Shape> sortShapes(List<Shape> shapes){
+		ArrayList<Shape> s1 = new ArrayList<Shape>();
+		ArrayList<Shape> s2 = new ArrayList<Shape>();
+		ArrayList<Shape> s3 = new ArrayList<Shape>();
+		ArrayList<Shape> newShapes = new ArrayList<Shape>();
 		
+		if(shapes.size() <= 1) {
+			return shapes;
+		}
+		
+		int pivot = shapes.size() / 2;
+		Shape pShape = shapes.get(pivot);
+		for(Shape s: shapes) {
+			if(s.compareTo(pShape) > 0) {
+				s3.add(s);
+			} else if(s.compareTo(pShape) < 0) {
+				s1.add(s);
+			} else {
+				s2.add(s);
+			}
+		}
+		
+		for(Shape s: sortShapes(s1)) {
+			newShapes.add(s);
+		}
+		for(Shape s: s2) {
+			newShapes.add(s);
+		}
+		for(Shape s: sortShapes(s3)) {
+			newShapes.add(s);
+		}
+		
+		return newShapes;
 	}
 }
